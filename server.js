9dd0899168f4ba33467c29d0d44a3e72ea6f6cb6
Liz Khoo <emkhoo@gmail.com>
2013-11-05 13:55:32 -0500

    status colors working

--- server.js
+++ server.js
@@ -26,17 +26,14 @@ function requestHandler(req, res) {
 // WebSockets work with the HTTP server
 var io = require('socket.io').listen(httpServer);
 var clients = new Array();
-var ifseeall = false;
-
-var activeuser = 0; // index of active user
-pacg_ = new Array();
+var activeuser = 0;
+var pacg_ = new Array();
 var allnicks = new Array();
-
 var lengt = 0;
-// Register a callback function to run when we have an individual connection
-// This is run for each individual user that connects
+
+
 io.sockets.on('connection', 
-	// We are given a websocket object in our function
+	
 	function (socket) {
 	
 		console.log("We have a new client: " + socket.id);
@@ -44,57 +41,43 @@ io.sockets.on('connection',
         var usernumber = null;
         usernumber = clients.length;
 		clients[usernumber] = socket.id;
-
-		// When this user "send" from clientside javascript, we get a "message"
-		// client side: socket.send("the message");  or socket.emit('message', "the message");
 		socket.emit('set_id', usernumber);
 		
 		socket.on('addnick', function(nick){
 			socket.nick = nick;
 			allnicks.push(nick);
 			socket.broadcast.emit('addnewuser', nick);
-			socket.emit('fulluserlist', allnicks);
+			socket.emit('fulluserlist', {users: allnicks, turn: activeuser});
 		})
 
         socket.on('message', 
 			// Run this function when a message is sent
 			function (messagetxt) {
-				console.log("message: " + messagetxt);
+				console.log("message received: " + messagetxt);
 				
-				// To all clients, on io.sockets instead
 				pacg_.push(messagetxt);
 				lengt = pacg_.length;
-				
-				//data.thismessage = message
-				
-				io.sockets.emit('fullmessage', {themessage: messagetxt, thelength: lengt, thenicknames: allnicks});
-                               // io.sockets.emit('pacag',lengt);
-				console.log("pacag's lengt" + lengt);
-			
+
+				activeuser++;
+				io.sockets.emit('updatemessage', {themessage: messagetxt, thelength: lengt, thenicknames: allnicks, turn: activeuser});
+				io.sockets.emit('fulluserlist', {users: allnicks, turn: activeuser});
 			}
 		);
     
-           socket.on('seeall', function(data){
-		      io.sockets.emit('seeall',pacg_);
-	       });
+       socket.on('seeall', function(data){
+	      io.sockets.emit('seeall',pacg_);
+       });
 
 		
 		socket.on('disconnect', function() {
-			
 			console.log(socket.nick + " has disconnected");
-
-
 			for (var i = 0; i < allnicks.length; i++){
 				if (socket.nick == allnicks[i]){
 					allnicks.splice(i,1);
 				}
 			} 
-			
 			io.sockets.emit('fulluserlist', allnicks);
-			//socket.broadcast.emit('fulluserlist', 'SERVER', socket.nick + ' has disconnected');
-
-			console.log(allnicks);
-			
+				
 		});
 
 	});
